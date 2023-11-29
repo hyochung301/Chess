8e0dcea7d4213bbca86fc8d4c9839cb0f96c1071
Sam(Hyokwon) Chung <hyochung@utexas.edu>
2023-11-29 23:27:37 +0900

    Pieces moving, showing pink

--- src/ChessBoard.java
+++ src/ChessBoard.java
@@ -1,5 +1,390 @@
-public class Main {
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+import javax.swing.border.*;
+//======================================================Don't modify below===============================================================//
+enum PieceType {king, queen, bishop, knight, rook, pawn, none}
+enum PlayerColor {black, white, none}
+
+public class ChessBoard {
+    private final JPanel gui = new JPanel(new BorderLayout(3, 3));
+    private JPanel chessBoard;
+    private JButton[][] chessBoardSquares = new JButton[8][8];
+    private Piece[][] chessBoardStatus = new Piece[8][8];
+    private ImageIcon[] pieceImage_b = new ImageIcon[7];
+    private ImageIcon[] pieceImage_w = new ImageIcon[7];
+    private JLabel message = new JLabel("Enter Reset to Start");
+
+    ChessBoard(){
+        initPieceImages();
+        initBoardStatus();
+        initializeGui();
+    }
+
+    public final void initBoardStatus(){
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++) chessBoardStatus[j][i] = new Piece();
+        }
+    }
+
+    public final void initPieceImages(){
+        pieceImage_b[0] = new ImageIcon(new ImageIcon("./img/king_b.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_b[1] = new ImageIcon(new ImageIcon("./img/queen_b.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_b[2] = new ImageIcon(new ImageIcon("./img/bishop_b.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_b[3] = new ImageIcon(new ImageIcon("./img/knight_b.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_b[4] = new ImageIcon(new ImageIcon("./img/rook_b.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_b[5] = new ImageIcon(new ImageIcon("./img/pawn_b.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_b[6] = new ImageIcon(new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
+
+        pieceImage_w[0] = new ImageIcon(new ImageIcon("./img/king_w.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_w[1] = new ImageIcon(new ImageIcon("./img/queen_w.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_w[2] = new ImageIcon(new ImageIcon("./img/bishop_w.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_w[3] = new ImageIcon(new ImageIcon("./img/knight_w.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_w[4] = new ImageIcon(new ImageIcon("./img/rook_w.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_w[5] = new ImageIcon(new ImageIcon("./img/pawn_w.png").getImage().getScaledInstance(64, 64, java.awt.Image.SCALE_SMOOTH));
+        pieceImage_w[6] = new ImageIcon(new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
+    }
+
+    public ImageIcon getImageIcon(Piece piece){
+        if(piece.color.equals(PlayerColor.black)){
+            if(piece.type.equals(PieceType.king)) return pieceImage_b[0];
+            else if(piece.type.equals(PieceType.queen)) return pieceImage_b[1];
+            else if(piece.type.equals(PieceType.bishop)) return pieceImage_b[2];
+            else if(piece.type.equals(PieceType.knight)) return pieceImage_b[3];
+            else if(piece.type.equals(PieceType.rook)) return pieceImage_b[4];
+            else if(piece.type.equals(PieceType.pawn)) return pieceImage_b[5];
+            else return pieceImage_b[6];
+        }
+        else if(piece.color.equals(PlayerColor.white)){
+            if(piece.type.equals(PieceType.king)) return pieceImage_w[0];
+            else if(piece.type.equals(PieceType.queen)) return pieceImage_w[1];
+            else if(piece.type.equals(PieceType.bishop)) return pieceImage_w[2];
+            else if(piece.type.equals(PieceType.knight)) return pieceImage_w[3];
+            else if(piece.type.equals(PieceType.rook)) return pieceImage_w[4];
+            else if(piece.type.equals(PieceType.pawn)) return pieceImage_w[5];
+            else return pieceImage_w[6];
+        }
+        else return pieceImage_w[6];
+    }
+
+    public final void initializeGui(){
+        gui.setBorder(new EmptyBorder(5, 5, 5, 5));
+        JToolBar tools = new JToolBar();
+        tools.setFloatable(false);
+        gui.add(tools, BorderLayout.PAGE_START);
+        JButton startButton = new JButton("Reset");
+        startButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                initiateBoard();
+            }
+        });
+
+        tools.add(startButton);
+        tools.addSeparator();
+        tools.add(message);
+
+        chessBoard = new JPanel(new GridLayout(0, 8));
+        chessBoard.setBorder(new LineBorder(Color.BLACK));
+        gui.add(chessBoard);
+        ImageIcon defaultIcon = new ImageIcon(new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
+        Insets buttonMargin = new Insets(0,0,0,0);
+        for(int i=0; i<chessBoardSquares.length; i++) {
+            for (int j = 0; j < chessBoardSquares[i].length; j++) {
+                JButton b = new JButton();
+                b.addActionListener(new ButtonListener(i, j));
+                b.setMargin(buttonMargin);
+                b.setIcon(defaultIcon);
+                if((j % 2 == 1 && i % 2 == 1)|| (j % 2 == 0 && i % 2 == 0)) b.setBackground(Color.WHITE);
+                else b.setBackground(Color.gray);
+                b.setOpaque(true);
+                b.setBorderPainted(false);
+                chessBoardSquares[j][i] = b;
+            }
+        }
+
+        for (int i=0; i < 8; i++) {
+            for (int j=0; j < 8; j++) chessBoard.add(chessBoardSquares[j][i]);
+
+        }
+    }
+
+    public final JComponent getGui() {
+        return gui;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                ChessBoard cb = new ChessBoard();
+                JFrame f = new JFrame("Chess");
+                f.add(cb.getGui());
+                f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                f.setLocationByPlatform(true);
+                f.setResizable(false);
+                f.pack();
+                f.setMinimumSize(f.getSize());
+                f.setVisible(true);
+            }
+        };
+        SwingUtilities.invokeLater(r);
+    }
+
+    //================================Utilize these functions========================================//
+
+    class Piece{
+        PlayerColor color;
+        PieceType type;
+
+        Piece(){
+            color = PlayerColor.none;
+            type = PieceType.none;
+        }
+        Piece(PlayerColor color, PieceType type){
+            this.color = color;
+            this.type = type;
+        }
+    }
+
+    public void setIcon(int x, int y, Piece piece){
+        chessBoardSquares[y][x].setIcon(getImageIcon(piece));
+        chessBoardStatus[y][x] = piece;
+    }
+
+    public Piece getIcon(int x, int y){
+        return chessBoardStatus[y][x];
+    }
+
+    public void markPosition(int x, int y){
+        chessBoardSquares[y][x].setBackground(Color.pink);
+    }
+
+    public void unmarkPosition(int x, int y){
+        if((y % 2 == 1 && x % 2 == 1)|| (y % 2 == 0 && x % 2 == 0)) chessBoardSquares[y][x].setBackground(Color.WHITE);
+        else chessBoardSquares[y][x].setBackground(Color.gray);
+    }
+
+    public void setStatus(String inpt){
+        message.setText(inpt);
+    }
+
+    public void initiateBoard(){
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++) setIcon(i, j, new Piece());
+        }
+        setIcon(0, 0, new Piece(PlayerColor.black, PieceType.rook));
+        setIcon(0, 1, new Piece(PlayerColor.black, PieceType.knight));
+        setIcon(0, 2, new Piece(PlayerColor.black, PieceType.bishop));
+        setIcon(0, 3, new Piece(PlayerColor.black, PieceType.queen));
+        setIcon(0, 4, new Piece(PlayerColor.black, PieceType.king));
+        setIcon(0, 5, new Piece(PlayerColor.black, PieceType.bishop));
+        setIcon(0, 6, new Piece(PlayerColor.black, PieceType.knight));
+        setIcon(0, 7, new Piece(PlayerColor.black, PieceType.rook));
+        for(int i=0;i<8;i++){
+            setIcon(1, i, new Piece(PlayerColor.black, PieceType.pawn));
+            setIcon(6, i, new Piece(PlayerColor.white, PieceType.pawn));
+        }
+        setIcon(7, 0, new Piece(PlayerColor.white, PieceType.rook));
+        setIcon(7, 1, new Piece(PlayerColor.white, PieceType.knight));
+        setIcon(7, 2, new Piece(PlayerColor.white, PieceType.bishop));
+        setIcon(7, 3, new Piece(PlayerColor.white, PieceType.queen));
+        setIcon(7, 4, new Piece(PlayerColor.white, PieceType.king));
+        setIcon(7, 5, new Piece(PlayerColor.white, PieceType.bishop));
+        setIcon(7, 6, new Piece(PlayerColor.white, PieceType.knight));
+        setIcon(7, 7, new Piece(PlayerColor.white, PieceType.rook));
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++) unmarkPosition(i, j);
+        }
+        onInitiateBoard();
+    }
+//======================================================Don't modify above==============================================================//
+
+
+
+
+    //======================================================Implement below=================================================================//
+    enum MagicType {MARK, CHECK, CHECKMATE};
+    private int selX, selY;
+    private boolean check, checkmate, end;
+    private PlayerColor turn = PlayerColor.black;
+
+    class ButtonListener implements ActionListener{
+        int x;
+        int y;
+        ButtonListener(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+        public void actionPerformed(ActionEvent e) {
+            Piece selectedPiece = getIcon(x, y);
+            if(selectedPiece.color == turn) {
+                // Unmark the old selected piece's possible moves
+                if(selX != -1 && selY != -1) {
+                    unmarkPossibleMoves(selX, selY);
+                }
+                // Mark the new selected piece's possible moves
+                markPossibleMoves(x, y);
+                selX = x;
+                selY = y;
+            } else if(selX != -1 && selY != -1) {
+                Piece pieceToMove = getIcon(selX, selY);
+                if(isValidMove(pieceToMove, selX, selY, x, y)) {
+                    movePiece(selX, selY, x, y);
+                    // Unmark the moved piece's possible moves
+                    unmarkPossibleMoves(selX, selY);
+                    selX = -1;
+                    selY = -1;
+                    turn = (turn == PlayerColor.black) ? PlayerColor.white : PlayerColor.black;
+                    check = isCheck(turn);
+                    checkmate = isCheckmate(turn);
+                    updateStatus();
+                } else {
+                    // Unmark the selected piece's possible moves if an invalid move was attempted
+                    unmarkPossibleMoves(selX, selY);
+                    selX = -1;
+                    selY = -1;
+                }
+            }
+        }
+    }
+
+    void markPossibleMoves(int x, int y) {
+        Piece piece = getIcon(x, y);
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                if(isValidMove(piece, x, y, i, j)) {
+                    markPosition(i, j);
+                }
+            }
+        }
+    }
+
+    void unmarkPossibleMoves(int x, int y) {
+        Piece piece = getIcon(x, y);
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                if(isValidMove(piece, x, y, i, j)) {
+                    unmarkPosition(i, j);
+                }
+            }
+        }
+    }
+
+    void movePiece(int startX, int startY, int endX, int endY) {
+        Piece pieceToMove = getIcon(startX, startY);
+        setIcon(endX, endY, pieceToMove);
+        setIcon(startX, startY, new Piece());
+    }
+
+    class Rook extends Piece {
+        Rook(PlayerColor color) {
+            this.color = color;
+            this.type = PieceType.rook;
+        }
+        boolean isValidMove(int startX, int startY, int endX, int endY) {
+            return (startX == endX || startY == endY);
+        }
+    }
+
+    class Bishop extends Piece {
+        Bishop(PlayerColor color) {
+            this.color = color;
+            this.type = PieceType.bishop;
+        }
+        boolean isValidMove(int startX, int startY, int endX, int endY) {
+            return Math.abs(startX - endX) == Math.abs(startY - endY);
+        }
+    }
+
+    class Queen extends Piece {
+        Queen(PlayerColor color) {
+            this.color = color;
+            this.type = PieceType.queen;
+        }
+        boolean isValidMove(int startX, int startY, int endX, int endY) {
+            return (startX == endX || startY == endY) || Math.abs(startX - endX) == Math.abs(startY - endY);
+        }
+    }
+
+    class King extends Piece {
+        King(PlayerColor color) {
+            this.color = color;
+            this.type = PieceType.king;
+        }
+        boolean isValidMove(int startX, int startY, int endX, int endY) {
+            return Math.abs(startX - endX) <= 1 && Math.abs(startY - endY) <= 1;
+        }
+    }
+
+    class Knight extends Piece {
+        Knight(PlayerColor color) {
+            this.color = color;
+            this.type = PieceType.knight;
+        }
+        boolean isValidMove(int startX, int startY, int endX, int endY) {
+            return (Math.abs(startX - endX) == 2 && Math.abs(startY - endY) == 1) || (Math.abs(startX - endX) == 1 && Math.abs(startY - endY) == 2);
+        }
+    }
+
+    class Pawn extends Piece {
+        Pawn(PlayerColor color) {
+            this.color = color;
+            this.type = PieceType.pawn;
+        }
+        boolean isValidMove(int startX, int startY, int endX, int endY) {
+            return (color == PlayerColor.white && endY - startY == 1 && startX == endX) || (color == PlayerColor.black && startY - endY == 1 && startX == endX);
+        }
+    }
+
+
+    void onInitiateBoard(){
+        selX = -1;
+        selY = -1;
+        check = false;
+        checkmate = false;
+        end = false;
+        turn = PlayerColor.black;
+        updateStatus();
+    }
+
+    boolean isValidMove(Piece piece, int startX, int startY, int endX, int endY) {
+        switch(piece.type) {
+            case rook:
+                return (startX == endX || startY == endY);
+            case bishop:
+                return Math.abs(startX - endX) == Math.abs(startY - endY);
+            case queen:
+                return (startX == endX || startY == endY) || Math.abs(startX - endX) == Math.abs(startY - endY);
+            case king:
+                return Math.abs(startX - endX) <= 1 && Math.abs(startY - endY) <= 1;
+            case knight:
+                return (Math.abs(startX - endX) == 2 && Math.abs(startY - endY) == 1) || (Math.abs(startX - endX) == 1 && Math.abs(startY - endY) == 2);
+            case pawn:
+                return (piece.color == PlayerColor.white && startY - endY == 1 && startX == endX) || (piece.color == PlayerColor.black && endY - startY == 1 && startX == endX);
+            default:
+                return false;
+        }
+    }
+
+    boolean isCheck(PlayerColor color) {
+        // Implement logic to check if the king of the given color is in check
+        return false;
+    }
+
+    boolean isCheckmate(PlayerColor color) {
+        // Implement logic to check if the king of the given color is in checkmate
+        return false;
+    }
+
+    void updateStatus() {
+        if(checkmate) {
+            setStatus(turn + " is in checkmate");
+            end = true;
+        } else if(check) {
+            setStatus(turn + " is in check");
+        } else {
+            setStatus(turn + "'s turn");
+        }
     }
 }
